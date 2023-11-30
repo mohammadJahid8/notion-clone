27198a78cc52986952d99b14a5e6f675b7f62167
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-11-30 20:56:51 +0600

    Implemented github and google authentication with convex and clerk

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -4,8 +4,17 @@ import { cn } from "@/lib/utils";
 import Logo from "./Logo";
 import { ModeToggle } from "@/components/mode-toggle";
 
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
+
 const Navbar = () => {
   const scrolled = useScrollTop();
+
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  console.log(isAuthenticated);
 
   return (
     <div
@@ -16,6 +25,28 @@ const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal" afterSignInUrl="/" afterSignUpUrl="/">
+              <Button variant="ghost" size="sm">
+                Login
+              </Button>
+            </SignInButton>
+
+            <SignInButton mode="modal" afterSignInUrl="/" afterSignUpUrl="/">
+              <Button size="sm">Get Jotion free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Jotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
