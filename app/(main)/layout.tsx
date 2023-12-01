372b7843e97aade22d546111c1015ff58f432acf
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-12-01 10:37:56 +0600

    [Done] Draggable sidebar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,6 +4,7 @@ import { Spinner } from "@/components/spinner";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
 import React from "react";
+import Navigation from "./_components/Navigation";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -19,6 +20,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div className="h-full flex dark:bg-[#1F1F1F]">
+      <Navigation />
       <main className="flex-1 h-full overflow-y-auto">{children}</main>
     </div>
   );
