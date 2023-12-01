f086c3b83d7c6c5819d8782228b0f6d5f7328d4d
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-12-01 14:23:39 +0600

    Added create a note image and button

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,38 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+
 const DocumentsPage = () => {
-  return <div>This is document page</div>;
+  const { user } = useUser();
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        width="300"
+        height="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        width="300"
+        height="300"
+        alt="Empty"
+        className="dark:block hidden"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName} &apos;s Jotion
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentsPage;
