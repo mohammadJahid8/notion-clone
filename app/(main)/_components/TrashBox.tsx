a88d3c74fc1727dc8333cc4db2abb556cf9b0523
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-12-02 10:08:39 +0600

    Implemented undo and remove api in trash

--- app/(main)/_components/TrashBox.tsx
+++ app/(main)/_components/TrashBox.tsx
@@ -9,6 +9,7 @@ import { toast } from "sonner";
 import { Spinner } from "@/components/spinner";
 import { Search, Trash, Undo } from "lucide-react";
 import { Input } from "@/components/ui/input";
+import ConfirmModal from "@/components/modals/confirm-modal";
 
 const TrashBox = () => {
   const router = useRouter();
@@ -92,12 +93,14 @@ const TrashBox = () => {
               >
                 <Undo className="text-muted-foreground h-4 w-4" />
               </div>
-              <div
-                role="button"
-                className="rounded-sm p-2 hover:bg-neutral-200"
-              >
-                <Trash className="text-muted-foreground h-4 w-4" />
-              </div>
+              <ConfirmModal onConfirm={() => onRemove(document._id)}>
+                <div
+                  role="button"
+                  className="rounded-sm p-2 hover:bg-neutral-200"
+                >
+                  <Trash className="text-muted-foreground h-4 w-4" />
+                </div>
+              </ConfirmModal>
             </div>
           </div>
         ))}
