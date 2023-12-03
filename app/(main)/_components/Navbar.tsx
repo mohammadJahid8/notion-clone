0c9b648e315ee1ce29e86bb993581763bdc973b6
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-12-03 07:25:33 +0600

    Added emoji and image toolbar and make the document body title editable

--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 
   return (
     <>
-      <nav className="bg-background px-3  py-2 w-full flex items-center gap-x-4">
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3  py-2 w-full flex items-center gap-x-4">
         {isCollapsed && (
           <MenuIcon
             role="butotn"
